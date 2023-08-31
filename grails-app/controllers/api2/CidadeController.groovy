c7e393011711b1b639835aceb5552116ff7e1468
jhartmann <jhartmann@insoft4.com>
2023-08-31 20:40:44 -0300

    feat: criar save da tabela cidade.

---
+++ grails-app/controllers/api2/CidadeController.groovy
@@ -0,0 +1,33 @@
+package api2
+
+import api2.command.CidadeCommand
+import grails.rest.*
+import grails.converters.*
+
+class CidadeController {
+
+    static responseFormats = ["json"]
+	static defaultAction = "get"
+    static allowedMethods = [
+            save: "POST",
+            list: "GET",
+            update: "PUT",
+            delete: "DELETE",
+            get: "GET"
+    ]
+
+    CidadeService cidadeService
+
+    def save(CidadeCommand command) {
+        Map retorno = cidadeService.save(command)
+        respond(retorno)
+    }
+
+    def list() {}
+
+    def update() {}
+
+    def delete() {}
+
+    def get() {}
+}
