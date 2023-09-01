10d69e7a340c6f9f5de2013a371fc0b4a03558d4
JoaoGH <gabrielhartmannqwert@gmail.com>
2023-09-01 09:22:21 -0300

    feat: realizar save do funcionario.

--- grails-app/controllers/api2/FuncionarioController.groovy
+++ grails-app/controllers/api2/FuncionarioController.groovy
@@ -7,14 +7,15 @@ class FuncionarioController implements ExceptionHandlers {
     static responseFormats = ['json']
     static defaultAction = "get"
     static allowedMethods = [
-
+            save: "POST"
     ]
 
     FuncionarioService funcionarioService
 
-    def save() {}
-
-    def list() {}
+    def save() {
+        Map retorno = funcionarioService.save()
+        respond(retorno)
+    }
 
     def update() {}
 
--- grails-app/services/api2/FuncionarioService.groovy
+++ grails-app/services/api2/FuncionarioService.groovy
@@ -1,11 +1,27 @@
 package api2
 
 import grails.gorm.transactions.Transactional
+import grails.web.api.ServletAttributes
 
 @Transactional
-class FuncionarioService {
+class FuncionarioService implements ServletAttributes {
 
-    def serviceMethod() {
+    Map save() {
+        Map retorno = [success: true]
 
+        Funcionario funcionario = new Funcionario()
+        funcionario.setNome(request.JSON.nome)
+
+        if (funcionario.hasErrors()) {
+            retorno.success = false
+            retorno.errors = funcionario.getErrors()
+            return retorno
+        }
+
+        funcionario.save(flush: true)
+
+        retorno.registro = funcionario
+
+        return retorno
     }
 }
