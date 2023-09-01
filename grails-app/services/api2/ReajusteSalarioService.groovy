4dd1e7d47fa75b814e04a98117fc41707f5e432b
jhartmann <jhartmann@insoft4.com>
2023-08-31 21:35:58 -0300

    feat: implemetar save para reajuste incluindo trattiva de data

---
+++ grails-app/services/api2/ReajusteSalarioService.groovy
@@ -0,0 +1,50 @@
+package api2
+
+import grails.gorm.transactions.Transactional
+import grails.web.api.ServletAttributes
+import javassist.NotFoundException
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+@Transactional
+class ReajusteSalarioService implements ServletAttributes {
+
+    Map save() {
+        Map retorno = [:]
+
+        Funcionario funcionario = Funcionario.get(request.JSON.idFuncionario)
+
+        if (!funcionario) {
+            throw new NotFoundException("Funcionario nao encontrado")
+        }
+
+        ReajusteSalario reajusteSalario = new ReajusteSalario()
+        reajusteSalario.setDataReajuste(getLocalDateByParameter(request.JSON.dataReajuste))
+        reajusteSalario.setValorSalario(request.JSON.valorSalario as BigDecimal)
+        reajusteSalario.setFuncionario(funcionario)
+        reajusteSalario.save(flush: true)
+
+        retorno.success = true
+        retorno.registro = getShowRecord(reajusteSalario)
+
+        return retorno
+    }
+
+    protected LocalDate getLocalDateByParameter(String parameter) {
+        return LocalDate.parse(parameter, DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+    }
+
+    protected Map getShowRecord(ReajusteSalario reajusteSalario) {
+        return [
+                id          : reajusteSalario.id,
+                dataReajuste: reajusteSalario.dataReajuste.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")),
+                valorSalario: reajusteSalario.valorSalario,
+                funcionario : [
+                        id: reajusteSalario.funcionario.id,
+                        nome: reajusteSalario.funcionario.nome
+                ]
+        ]
+    }
+
+}
